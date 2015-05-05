3609242a680d968ae9ae33fd0aabce9cf19c1046
cloud <cloud.chen@looper.hk>
2015-05-05 13:47:55 +0800

    更新camera.js

--- js/app/camera.js
+++ js/app/camera.js
@@ -1,4 +1,4 @@
-//2015.1.26
+//2015.05.05
 
 $(document).ready(function(e) {	
 	
@@ -12,16 +12,15 @@ $(document).ready(function(e) {
 	var imgBtnSub=imgPanel.children('a.btnSub');
 	var imgBtnR=imgPanel.children('a.btnR');
 	var imgBtnCf=$('a.btnCf');
-	var imgCv,imgCanvas,imgPic;
-	var imgStep=1;
-	var imgScale,imgSaleMin=0.1,imgScaleMax=1,imgScaleTimer;
-	if(isSamsung) imgScaleMax=0.55;
-	//if(isWeixin && os.android) imgScaleMax=2;
+	var imgCanvas,imgPic='imgPic';
+	var imgScale=0.5,imgSaleMin=0.1,imgScaleMax=2,imgScaleTimer;
 	
 	//拍照
 	var btnCamera=$('.btnCamera');
 	var btnCf=$('#btnCf');
 	var btnsBox=$('div.btns');
+	var imgBaby=$('img.baby');
+    var imgFocus=$('.focus');	
 	
 	//touch 
 	var mutiTouch;
@@ -30,30 +29,23 @@ $(document).ready(function(e) {
 	init();
 	
 	function init(){
-		fileBox=$('<div id="fileBox"><input type="file" accept="image/jpeg" name="fileInput"/></div>').appendTo(htmlBox);
+		fileBox=$('<div id="fileBox"><input type="file" capture="camera" accept="image/*" name="fileInput"/></div>').appendTo(htmlBox);
 		fileInput=fileBox.children();
+		imgShell.css({height:imgShell.width()*0.75});
 		addEvent();		
 	}//end func
 	
 	function addEvent(){
 		fileInput.on('change',fileSelected);
-		btnCamera.on('click',btnCamera_click);
-		imgBtnCf.on('click',imgBtnCf_click);
+		btnCamera.on('touchend',btnCamera_click);
+		imgBtnCf.on('touchend',imgBtnCf_click);
 	}//end func
-
+	
 	//---------------------------------------------------拍照事件
 	
 	//图片确定按钮，图片编辑步骤控制
 	function imgBtnCf_click(e){
-		if(imgStep==1){//第一步,调整刚刚拍摄或者打开的图片
-			imgStep++;
-			imgSend();
-		}//end if
-		else if(imgStep==2){//第二步，比如选择一个产品加入画布
-			imgStep++;
-		}//end else
-		else{//第三步，比如在画布里填写祝福文字
-		}//end else
+		imgSend();
 	}//end func
 	
 	//拍照按钮
@@ -82,29 +74,12 @@ $(document).ready(function(e) {
         if (file) {
 			loadBox.show();
 		  	var reader = new FileReader();  
-		  	reader.onloadend = function() { 
-				console.log('result.length: '+this.result.length);
-				console.log('file path: '+fileInput.val());
-				if(this.result.length>0){
-					//模拟ajax
-					setTimeout(imageLoad,1000,'images/photo.jpg',imgCreat);
-					
-					//ajax 先把base64图片数据发送到服务器，生成图片地址返回，然后执行imgCreat()
-					/*
-					var data=this.result.split(",");
-					$.post("webajax/handler.ashx", { data:data[1] },function(resp){
-                        console.log(resp.code,resp.src);
-						if(resp.code == 200){
-							var src=resp.src;
-							imageLoad("upload/"+src,imgCreat);
-						}//end if
-					}, "json");
-					*/
-				}//end if
-				else{
-					loadBox.hide();
-					alertFunc('无法正确读取图片');	
-				}//end else
+		  	reader.onloadend = function() {
+				loadBox.hide();
+				//console.log('result.length: '+this.result.length);
+				//console.log('file path: '+fileInput.val());
+				if(this.result.length>0) imgCreat(this.result);
+				else alertFunc('无法正确读取图片');
 		  	}//end onload	
 			reader.readAsDataURL(file);	
         }//end if
@@ -113,27 +88,22 @@ $(document).ready(function(e) {
 	//----------------------------------复制图片至canvas
 	function imgCreat(src){
 		loadBox.hide();
-		cameraBox.hide();
-		btnsBox.show();
-		canvasBox.show();
+		imgBaby.hide();
+		imgPanel.children('a').show();
+		btnCamera.children('img').attr({src:'images/camera/btn_redo.png'});
+		imgBtnCf.show();
 		imgShell.empty();
-		$('<canvas></canvas>').attr({width:imgShell.width(),height:imgShell.height()}).prependTo(imgShell);
-		imgCv=$('canvas');
-		imgCanvas = oCanvas.create({
-			canvas: imgCv[0]
-		});
-		imgPic = imgCanvas.display.image({
-			x:imgCv.width()/2,
-			y:imgCv.height()/2,
-			origin: { x: "center", y: "center" },
-			image: src,
-		});
-		imgCanvas.addChild(imgPic);
-		if(os.ios) imgScale=0.3;
-		else if(isSamsung) imgScale=imgScaleMax;
-		else imgScale=0.5;
+		imgCanvas=$('<canvas></canvas>').attr({width:imgShell.width(),height:imgShell.height()}).prependTo(imgShell);
+		imgCanvas.clearCanvas();
+		imgCanvas.drawImage({
+		  layer: true,
+		  name:imgPic,
+		  source: src,
+		  x: imgCanvas.width()/2, y: imgCanvas.height()/2,
+		  fromCenter: true
+		}).drawLayers();
+		imgScale=0.5;
 		imgScaleSet(imgPic);
-		if(os.android && isWeixin) setTimeout(function(){imgCv[0].style.opacity = "0.99";},100);
 		addImgEvent(imgPic);
 	}//end func
 	
@@ -143,7 +113,8 @@ $(document).ready(function(e) {
 		
 		//模拟一下发送图片然后跳转到结果页
 		setTimeout(function(){
-			window.location.href='stage.html';
+			window.location.href='result.html';
+			loadBox.hide();
 		},1000);
 		
 		/*
@@ -151,13 +122,25 @@ $(document).ready(function(e) {
 		var data=$('canvas')[0].toDataURL("image/png").split(","); //图片BASE64数据
 		$.post("handler.php?action=update", { data:data[1] },function(resp){
 			if(resp.code == 200){
-				window.location.href = 'result.html?photo=' + resp.src;
+				window.location.href='result.html?photo='+resp.src;
 			}
 			if(data.code == 12) $('#popAlert').popOn({text:'参数错误，请重新上传~',url:"index.html"});
 			if(data.code == 13) $('#popAlert').popOn({text:'图片数据错误，请重新上传~'});
 			if(data.code == 14) $('#popAlert').popOn({text:'数据库错误，请重新提交~'});
 		}, "json");
 		*/
+		//图片BASE64数据
+        var data=$('canvas')[0].toDataURL("image/png").split(",");
+		//头像取景框的左上角和右下角位置
+		var params={x:imgFocus.width()/2,y:imgFocus.position().top,width:imgFocus.width(),height:imgFocus.height()};
+		$.post("webajax/screenshot.ashx", { data:data[1],x:params.x,y:params.y,width:params.width,height:params.height },function(resp){
+			if(resp.code == 200){
+				window.location.href = 'select.html?photo=' + resp.src;
+			}
+			if(data.code == 12) $('#popAlert').popOn({text:'参数错误，请重新上传~',url:"index.html"});
+			if(data.code == 13) $('#popAlert').popOn({text:'图片数据错误，请重新上传~'});
+			if(data.code == 14) $('#popAlert').popOn({text:'数据库错误，请重新提交~'});
+		}, "json");
 		
 	}//end func
 	
@@ -192,7 +175,7 @@ $(document).ready(function(e) {
 			mutiTouch=false;
 			posLast1=[event.touches[0].clientX,event.touches[0].clientY];
 		}//end if
-		else if(event.touches.length>=2 && os.ios){
+		else if(event.touches.length>=2){
 			mutiTouch=true;
 			posLast1=[event.touches[0].clientX,event.touches[0].clientY];
 			posLast2=[event.touches[1].clientX,event.touches[1].clientY];
@@ -203,30 +186,37 @@ $(document).ready(function(e) {
 	
 	function imgShell_touchmove(e){
 		e.preventDefault();
+		var obj=e.data.obj;
 		if(!mutiTouch && event.touches.length==1){
-			var pos=[event.touches[0].clientX,event.touches[0].clientY];
-			e.data.obj.move(pos[0]-posLast1[0], pos[1]-posLast1[1]);
-			posLast1=pos;
-			imgCanvas.redraw();
-			if(os.android && isWeixin) imgCv[0].style.opacity = imgCv[0].style.opacity ? "" : "0.99";
+			var pos1=[event.touches[0].clientX,event.touches[0].clientY];
+			imgCanvas.setLayer(obj, {
+			  x:'+='+(pos1[0]-posLast1[0]),
+			  y:'+='+(pos1[1]-posLast1[1])
+			});
+			posLast1=pos1;
 		}//end if
-		else if(event.touches.length>=2 && os.ios){
+		else if(event.touches.length>=2){
 			var pos1=[event.touches[0].clientX,event.touches[0].clientY];
 			var pos2=[event.touches[1].clientX,event.touches[1].clientY];
 			var dis=getDis(pos1,pos2);
-			if(dis>disLast && Math.abs(dis-disLast)>0.5) imgScaleFunc(e.data.obj,1,0.01);
-			else if(dis<disLast && Math.abs(dis-disLast)>0.5) imgScaleFunc(e.data.obj,-1,0.01);
+			if(Math.abs(dis-disLast)>0.5){
+				imgScale+=0.01*(dis-disLast)/Math.abs(dis-disLast);
+				imgScale=imgScale<=imgSaleMin?imgSaleMin:imgScale;
+				imgScale=imgScale>=imgScaleMax?imgScaleMax:imgScale;
+				imgCanvas.setLayer(obj, {
+				  scale:imgScale
+				});
+			}//end if
 			var deg=getDeg(pos1,pos2);
-			var degDis=deg-degLast;
-			if( degDis>15 || degDis<-15) degDis+=180;
-			e.data.obj.rotate(degDis);
-			imgCanvas.redraw();
-			if(os.android && isWeixin) imgCv[0].style.opacity = imgCv[0].style.opacity ? "" : "0.99";
+			imgCanvas.setLayer(obj, {
+			  rotate:'+='+(deg-degLast)
+			});	
 			posLast1=pos1;
 			posLast2=pos2;
 			disLast=dis;
 			degLast=deg;
 		}//end if
+		imgCanvas.drawLayers();
 	}//end func
 
 	function imgShell_touchend(e){
@@ -247,25 +237,24 @@ $(document).ready(function(e) {
 	}//end func
 	
 	function imgScaleFunc(obj,offset,speed){
-		speed=speed!=null?speed:0.01;
+		speed=speed!=null?speed:0.05;
 		imgScale+=speed*offset;
-		imgScale=imgScale<=imgSaleMin?imgSaleMin:imgScale;
-		imgScale=imgScale>=imgScaleMax?imgScaleMax:imgScale;
 		imgScaleSet(obj);
 	}//end func
 	
 	function imgScaleSet(obj){
-		obj.scale(imgScale);
-		imgCanvas.redraw();
-		if(os.android && isWeixin) imgCv[0].style.opacity = imgCv[0].style.opacity ? "" : "0.99";
+		imgScale=imgScale<=imgSaleMin?imgSaleMin:imgScale;
+		imgScale=imgScale>=imgScaleMax?imgScaleMax:imgScale;
+		imgCanvas.setLayer(obj, {
+		  scale:imgScale
+		}).drawLayers();
 	}//end func
 	
 	//图片旋转
 	function imgBtnR_click(e){
-		e.data.obj.rotate(90);
-		imgCanvas.redraw();
-		if(os.android && isWeixin) imgCv[0].style.opacity = imgCv[0].style.opacity ? "" : "0.99";
+		imgCanvas.setLayer(e.data.obj, {
+		  rotate:'+=90'
+		}).drawLayers();
 	}//end func
-
 
 });
