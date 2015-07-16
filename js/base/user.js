968b5ba3a2878ee78b20c7cf029d7340746e792b
cloud <cloud.chen@looper.hk>
2015-07-16 11:25:57 +0800

    新增微信用户验证 user.js

---
+++ js/base/user.js
@@ -0,0 +1,35 @@
+//2015.7.16
+var loop = new Loop({ keyname: 'littlesheep-hotpot-h5', appkey: '', link: '' });
+var iuser=importUser();
+
+function importUser(){
+	
+	var user={};
+	user.info={};
+	
+	user.init=function(callback) {
+	    if(os.weixin){
+			loop.login(function () {
+			    loop.getuser(function (data) {
+			    	initCallback(data,callback);
+			    });
+			});
+		}//end if
+		else{
+			var data={headimage:'images/user.jpg',nickname:'user'};
+			initCallback(data,callback);
+		}//end else
+	}//end func	
+	
+	function initCallback(data,callback){
+		console.log('user info getted');
+		icom.objectPrint(data);
+		user.info=data;
+		if(callback) callback(data);
+	}//end func
+	
+	return user;
+	
+}//end func
+
+
