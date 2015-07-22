0a6f292cfc05b906ccdbf8658068aafe94b57673
cloud <cloud.chen@looper.hk>
2015-07-22 14:14:03 +0800

    更新 index.js

--- js/index.js
+++ js/index.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
 	
 	//-----------------------------------------定义和初始化变量----------------------------------------
 	var loadBox=$('aside.loadBox');
+	var articleBox=$('article');
 	
-	//----------------------------------------加载页面图片----------------------------------------
-	
-	icom.screenTo169(true,true);//article标签适配，参数1:iphone4拉伸至iphone5,参数2：虚拟系统工具栏（无实体按键）的安卓把高度压缩至一屏
+	//----------------------------------------页面初始化----------------------------------------
+	icom.screenTo169();//article标签高度适配，参数1:iphone4拉伸至iphone5，默认值：true；参数2：虚拟系统工具栏（无实体按键）的安卓把高度压缩至一屏，默认值：true
 	//icom.addSignBar('本页面为测试版本,抽奖结果无效!');
 	
+	//----------------------------------------微信用户登录验证----------------------------------------
+//	iuser.init(userGetted);
+//	
+//	function userGetted(data){
+//		console.log('用户头像：'+data.headimage);
+//		console.log('用户昵称：'+data.nickname);
+//		loadFunc();
+//	}//end func
+	
+	//----------------------------------------加载页面图片----------------------------------------
 	
 	loadFunc();
 	
@@ -21,7 +31,7 @@ $(document).ready(function(){
 		}); 	
 
 		loader.addCompletionListener(function() {
-			console.log('Load page images is completed!');
+			console.log('页面图片加载完毕');
 			//icom.fadeOut(loadBox,500);
 			init();
 			loader=null;
@@ -33,12 +43,13 @@ $(document).ready(function(){
 	//----------------------------------------页面逻辑代码----------------------------------------
 	
 	function init(){
-		add_monitor();
+		
 	}//end func
 	
 	//----------------------------------------页面监测代码----------------------------------------
-	function add_monitor(){
-		//imonitor.add({obj:$('#btnTest'),action:'touchend',category:'首页',label:'测试按钮'});
+	monitor_handler();
+	function monitor_handler(){
+		//imonitor.add({obj:$('a.btnTest'),action:'touchend',category:'首页',label:'测试按钮'});
 	}//end func
 	
 });//end ready
