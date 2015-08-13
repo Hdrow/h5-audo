ccb1933d55a528c24f14bbce4cff009bd4b06a39
cloud <cloud.chen@looper.hk>
2015-08-13 16:03:32 +0800

    更新 video插件

--- js/base/video.js
+++ js/base/video.js
@@ -1,21 +1,17 @@
-//2015.8.11
+//2015.8.13
 
 var ivideo=importVideo();
 
 function importVideo(){
-	var ios='system',btn;
 	var video={};
 	
-	video.on=function(option){
-		if(option && option.btn){
-			btn=option.btn;
-			if(option.ios) ios=option.ios;
-			if(!os.ios || (os.ios && ios!='system')) btn.on('touchend',android_play);
-			else ios_play();
-		}//end if
+	video.on=function(btn){
+		btn=btn||$('a.btnVideo,#btnVideo');
+		if(os.ios) ios_play(btn);
+		else btn.on('touchend',android_play);
 	}//end func
 
-	function ios_play(){
+	function ios_play(btn){
 		btn.each(function(i) {
 			var vid=$(this).data('vid');
 			if(vid && vid!=''){
@@ -28,7 +24,6 @@ function importVideo(){
 	}//end func
 	
 	function android_play(e){
-		var container;
 		var box=$("<aside class='videoBox' id='videoBox'><a class='close'></a></aside>").show().appendTo($('body'));
 		var vid=$(this).data('vid');
 		if(vid && vid!=''){
@@ -36,9 +31,11 @@ function importVideo(){
 			type=type||'youku';
 			var ht=$(window).width()*9/16;
 			var top=$(window).height()/2-ht/2;
-			if(type=='youku') container=$('<iframe src="http://player.youku.com/embed/'+vid+'" frameborder=0 allowfullscreen isAutoPlay="true"></iframe>').css({height:ht,top:top}).prependTo(box);
+			if(type=='youku'){
+				var container=$('<iframe src="http://player.youku.com/embed/'+vid+'" frameborder=0 allowfullscreen isAutoPlay="true"></iframe>').css({height:ht,top:top}).prependTo(box);
+			}//end if
 			else{
-				container=$('<video type="video/mp4" controls autoplay>').attr({src:vid}).css({height:ht,top:top}).prependTo(box);
+				var container=$('<video type="video/mp4" controls autoplay>').attr({src:vid}).css({height:ht,top:top}).prependTo(box);
 				videoPoser=$(this).data('poster');
 				if(videoPoser) container.attr({poster:videoPoser});
 				container[0].play();
