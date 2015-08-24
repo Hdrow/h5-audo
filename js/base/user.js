b5ad07fd78e2207309e2dd09a985416b8ba4ec10
cloud <cloud.chen@looper.hk>
2015-08-24 14:54:52 +0800

    更新 user.js

--- js/base/user.js
+++ js/base/user.js
@@ -1,4 +1,4 @@
-//2015.7.16
+//2015.8.24
 var loop = new Loop({ keyname: 'loop_test', appkey: '', link: '' });
 var iuser=importUser();
 
@@ -8,21 +8,15 @@ function importUser(){
 	user.info={};
 	
 	user.init=function(callback) {
-	    if(os.weixin){
-			loop.login(function () {
-			    loop.getuser(function (data) {
-			    	initCallback(data,callback);
-			    });
-			});
-		}//end if
-		else{
-			var data={headimage:'images/user.jpg',nickname:'user'};
-			initCallback(data,callback);
-		}//end else
+	    loop.login(function () {
+		    loop.getuser(function (data) {
+		    	initCallback(data,callback);
+		    });
+		});
 	}//end func	
 	
 	function initCallback(data,callback){
-		console.log('user info getted');
+		console.log('获得微信用户数据');
 		icom.objectPrint(data);
 		user.info=data;
 		if(callback) callback(data);
