c44b1bca2ee0e2a684918507d5eeb91f1535fce1
cloud <cloud.chen@looper.hk>
2015-08-28 11:45:41 +0800

    更新video插件，iphone6和plus优先调用浏览器内部播放而不是系统播放器

--- js/base/video.js
+++ js/base/video.js
@@ -1,4 +1,4 @@
-//2015.8.13
+//2015.8.28
 
 var ivideo=importVideo();
 
@@ -7,23 +7,23 @@ function importVideo(){
 	
 	video.on=function(btn){
 		btn=btn||$('a.btnVideo,#btnVideo');
-		if(os.ios) ios_play(btn);
-		else btn.on('touchend',android_play);
+		if(os.iphone4 || os.iphone5) btn_play(btn);
+		else btn.on('touchend',pop_play);
 	}//end func
 
-	function ios_play(btn){
+	function btn_play(btn){
 		btn.each(function(i) {
 			var vid=$(this).data('vid');
 			if(vid && vid!=''){
 				var type=$(this).data('type');
 				type=type||'youku';
 				if(type=='youku') $('<iframe src="http://player.youku.com/embed/'+vid+'" frameborder=0 allowfullscreen></iframe>').appendTo($(this));
-				else $('<video type="video/mp4">').attr({src:vid}).appendTo($(this));
+				else  container=$('<video type="video/mp4" controls>').attr({src:vid}).appendTo($(this));
 			}//end if
 		});
 	}//end func
 	
-	function android_play(e){
+	function pop_play(e){
 		var box=$("<aside class='videoBox' id='videoBox'><a class='close'></a></aside>").show().appendTo($('body'));
 		var vid=$(this).data('vid');
 		if(vid && vid!=''){
@@ -31,13 +31,11 @@ function importVideo(){
 			type=type||'youku';
 			var ht=$(window).width()*9/16;
 			var top=$(window).height()/2-ht/2;
-			if(type=='youku'){
-				var container=$('<iframe src="http://player.youku.com/embed/'+vid+'" frameborder=0 allowfullscreen isAutoPlay="true"></iframe>').css({height:ht,top:top}).prependTo(box);
-			}//end if
+			if(type=='youku') $('<iframe src="http://player.youku.com/embed/'+vid+'" frameborder=0 allowfullscreen isAutoPlay="true"></iframe>').css({height:ht,top:top}).prependTo(box);
 			else{
-				var container=$('<video type="video/mp4" controls autoplay>').attr({src:vid}).css({height:ht,top:top}).prependTo(box);
-				videoPoser=$(this).data('poster');
-				if(videoPoser) container.attr({poster:videoPoser});
+				var container=$('<video type="video/mp4" controls preload="auto" webkit-playsinline="true">').attr({src:vid}).css({height:ht,top:top}).prependTo(box);
+				var poster=$(this).data('poster');
+				if(poster) container.attr({poster:poster});
 				container[0].play();
 			}//end else
 		}//end if
@@ -48,4 +46,3 @@ function importVideo(){
 	
 	return video;
 }//end import
-
