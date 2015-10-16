dcac4ba6b367f89f207e078a9cb279e56c17a7c5
cloud <cloud.chen@looper.hk>
2015-10-16 16:21:16 +0800

    更新 video audio 插件

--- js/base/audio.js
+++ js/base/audio.js
@@ -1,11 +1,11 @@
-//2015.7.7
+//2015.10.16
 var iaudio=importAudio();
 
 function importAudio(){
 	var audio={};
 	
 	audio.on=function(option){
-		if(option && option.src && option.src!=""){
+		if(option.src){
 			var sound=new Audio();
 			sound.src=option.src;
 			sound.loop=option.loop!=null?option.loop:false;//如果loop设置成true就无法正确获得ended事件
@@ -17,26 +17,20 @@ function importAudio(){
 			var onEnded=option.onEnded;
 			var onPlay=option.onPlay;
 			var onPause=option.onPause;
+			var onTimeupdate=option.onTimeupdate;
 			sound.load();
-			sound.addEventListener('loadstart',sound_loadstart,false);
+			if(onLoadstart) sound.addEventListener('loadstart',onLoadstart,false);
 			sound.addEventListener('loadeddata',sound_onloadeddata,false);
-			sound.addEventListener('ended',sound_ended,false);
-			sound.addEventListener('timeupdate',sound_timeupdate,false);
-			sound.addEventListener('play',sound_play,false);
-			sound.addEventListener('pause',sound_pause,false);
+			if(onEnded) sound.addEventListener('ended',onEnded,false);
+			if(onTimeupdate) sound.addEventListener('timeupdate',onTimeupdate,false);
+			if(onPlay) sound.addEventListener('play',onPlay,false);
+			if(onPause) sound.addEventListener('pause',onPause,false);
 			return sound;
 		}//end if
 		
-		function sound_loadstart(event){
-			var src=get_src(this.src);
-			console.log(src+' loadstart');
-			if(onLoadstart) onLoadstart(this);
-		}//end func
-		
 		function sound_onloadeddata(event){
 			var src=get_src(this.src);
-			console.log(src+' canplaythrough');
-			console.log(src+' duration:'+this.duration);
+			console.log(src+' loaded');
 			if(onLoaded) onLoaded(this);
 			if(autoplay){
 				this.volume=volume;
@@ -44,50 +38,7 @@ function importAudio(){
 				this.play();
 			}//end if
 		}//end func
-		
-		function sound_ended(event){
-			var src=get_src(this.src);
-			console.log(src+' ended');
-			if(onEnded) onEnded(this);
-		}//end func
-		
-		function sound_timeupdate(event){
-			//console.log('audio currentTime:'+this.currentTime);
-		}//end func
-		
-		function sound_play(event){
-			var src=get_src(this.src);
-			console.log(src+' play');
-			if(onPlay) onPlay(this);
-		}//end func
-		
-		function sound_pause(event){
-			var src=get_src(this.src);
-			console.log(src+' pause');
-			if(onPause) onPause(this);
-		}//end func
-		
-		function get_src(str){
-			var ary=str.split('/');
-			return ary[ary.length-1];
-		}//end func
-		
-	}//end func
-	
-	audio.play=function(sound,volume){
-		if(sound){
-			volume=volume!=null?volume:1;
-			console.log('volume:'+volume);
-			sound.volume=volume;
-			sound.play();
-		}//end if
-	}//end func
-	
-	audio.stop=function(sound){
-		if(sound){
-			sound.currentTime=0;
-			sound.pause();
-		}//end if		
+				
 	}//end func
 	
 	audio.fadeIn=function(option){
@@ -124,6 +75,11 @@ function importAudio(){
 			setTimeout(audio.fadeOut,100,option);
 		}//end if
 		else if(onComplete) onComplete();
+	}//end func
+	
+	function get_src(str){
+		var ary=str.split('/');
+		return ary[ary.length-1];
 	}//end func
 	
 	return audio;
--- js/base/video.js
+++ js/base/video.js
@@ -1,43 +1,78 @@
-//2015.9.2
+//2015.10.16
 
 var ivideo=importVideo();
 
 function importVideo(){
 	var video={};
 	
-	video.on=function(btn){
-		btn=btn||$('a.btnVideo,#btnVideo');
-		if(os.ios) ios_play(btn);
-		else btn.on('touchend',pop_play);
+	video.add=function(option){
+		var vid;
+		if(option.src){
+			var src=option.src;
+			var shell=option.shell||$('body');
+			var classname=option.classname;
+			var controls=option.controls!=null?option.controls:false;
+			if(os.iphone4) controls=true;
+			var autoplay=option.autoplay!=null?option.autoplay:true;
+			var poster=option.poster;
+			var onLoadstart=option.onLoadstart;
+			var onLoaded=option.onLoaded;
+			var onEnded=option.onEnded;
+			var onPlay=option.onPlay;
+			var onPause=option.onPause;
+			var onTimeupdate=option.onTimeupdate;
+			vid=$('<video webkit-playsinline="true" type="video/mp4">').attr({src:src,autoplay:autoplay,controls:controls,poster:poster}).appendTo(shell);
+			if(classname) vid.addClass(classname);
+			if(controls) vid.attr({controls:controls});
+			if(onLoadstart) vid[0].addEventListener('loadstart',onLoadstart,false);
+			if(onLoaded) vid[0].addEventListener('loadeddata',onLoaded,false);
+			if(onEnded) vid[0].addEventListener('ended',onEnded,false);
+			if(onTimeupdate) vid[0].addEventListener('timeupdate',onTimeupdate,false);
+			if(onPlay) vid[0].addEventListener('play',onPlay,false);
+			if(onPause) vid[0].addEventListener('pause',onPause,false);
+			return vid;
+		}//end if
 	}//end func
 	
-	function ios_play(btn){
+	video.on=function(option){
+		var btn=option.btn||$('a.btnVideo,#btnVideo');
+		var callback={onEnded:option.onEnded};
+		if(os.ios) ios_play(btn,callback);
+		else btn.on('touchend',callback,pop_play);
+	}//end func
+	
+	function ios_play(btn,callback){
 		btn.each(function(i) {
 			var vid=$(this).data('vid');
 			if(vid && vid!=''){
 				var type=$(this).data('type');
 				type=type||'youku';
 				if(type=='youku') btn_play($(this));
-				else if(type=='qq'){
-					$(this).on('touchend',pop_play);
-				}//end if
-				else if(type=='mp4') btn_play($(this));
+				else if(type=='qq') $(this).on('touchend',{},pop_play);
+				else if(type=='mp4') btn_play($(this),callback);
 			}//end if
 		});
 	}//end func
 
-	function btn_play(btn){
+	function btn_play(btn,callback){
 		var vid=btn.data('vid');
 		if(vid && vid!=''){
 			var type=btn.data('type');
 			type=type||'youku';
 			if(type=='youku') $('<iframe src="http://player.youku.com/embed/'+vid+'" frameborder=0 allowfullscreen></iframe>').appendTo(btn);
 			else if(type=='qq') $('<iframe src="http://v.qq.com/iframe/player.html?vid='+vid+'&tiny=0&auto=0" frameborder="0" allowfullscreen></iframe>').appendTo(btn);
-			else container=$('<video type="video/mp4">').attr({src:vid}).appendTo(btn);
+			else if(type=='mp4'){
+				container=$('<video type="video/mp4">').attr({src:vid}).appendTo(btn);
+				console.log(container[0]);
+				if(callback){
+					if(callback.onEnded) container[0].addEventListener('ended',callback.onEnded,false);
+				}//end if
+			}//end else if
 		}//end if
 	}//end func
 	
 	function pop_play(e){
+		var callback=e.data;
 		var box=$("<aside class='videoBox' id='videoBox'><a class='close'></a></aside>").show().appendTo($('body'));
 		var vid=$(this).data('vid');
 		if(vid && vid!=''){
@@ -52,6 +87,9 @@ function importVideo(){
 				var poster=$(this).data('poster');
 				if(poster) container.attr({poster:poster});
 				container[0].play();
+				if(callback){
+					if(callback.onEnded) container[0].addEventListener('ended',callback.onEnded,false);
+				}//end if
 			}//end else
 		}//end if
 		box.children('a.close').one('touchend',function(e){
