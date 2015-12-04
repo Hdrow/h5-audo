070ff135dbe07ac4a0ede7481d850ebc5a0b975d
cloud <cloud.chen@looper.hk>
2015-12-04 10:04:05 +0800

    更新 monitor.js

--- js/base/monitor.js
+++ js/base/monitor.js
@@ -1,6 +1,8 @@
-//2015.11.30
+//2015.12.4
 var _hmt = _hmt || [];
 var imonitor=importMonitor();
+
+//pm给的监测代码贴在这里
 
 function importMonitor(){
 	var monitor={};
@@ -8,24 +10,25 @@ function importMonitor(){
 	monitor.add=function(option){
 		if(option){
 			var obj=option.obj;
-			var category=option.category||'页面监测';
+			var category=option.category||'';
 			var action=option.action||'click';
 			var label=option.label||'';
+			console.log(category)
 			if(obj && obj.length>0){
 				obj.each(function(i) {
-					$(this).on(action,{category:category,action:action,label:obj.length==1?label:label+(i+1)},event_handler);}
+					$(this).on(action,{category:category,action:action,label:obj.length==1?label:label+(i+1)},event_bind);}
 				);
 			}//end if
-			else if(label && label!='') monitorHandler({category:category,action:'JS程序触发',label:label});
+			else event_bind(null,{category:category,action:'程序触发',label:label});
 		}//end if
 	}//end func
 	
-	function event_handler(e){
-		var data=e.data;
-		monitor.event(data);
+	function event_bind(e,data){
+		if(e) event_handler(e.data);
+		else event_handler(data);
 	}//end func
 	
-	monitor.event=function(data){
+	function event_handler(data){
 		_hmt.push(['_trackEvent', data.category, data.action, data.category+'——'+data.label]);
 		console.log('项目类别：'+data.category+' | '+'交互行为：'+data.action+' | '+'项目说明：'+data.category+'——'+data.label);
 	}//end func
