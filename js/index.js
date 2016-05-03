1d99d286e8ca0ce7f7c83e3f38d941246d887335
cloud <cloud.chen@looper.hk>
2016-05-03 10:39:49 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -28,16 +28,28 @@ $(document).ready(function(){
 		var loader = new PxLoader();
 		loader.addImage('images/common/turn.gif');
 		
+		//实际加载进度
 //		loader.addProgressListener(function(e) {
-//			var per=Math.round(e.completedCount/e.totalCount*100);
+//			var per=Math.round(e.completedCount/e.totalCount*50);
+//			loadPer.css({height:per+'%'});
 //		}); 	
 
 		loader.addCompletionListener(function() {
 			init_handler();
+			//load_timer(50);//模拟加载进度
 			loader=null;
 		});
 		loader.start();	
-	}//end func	
+	}//end func
+	
+	function load_timer(per){
+		per=per||0;
+		per+=imath.randomRange(1,3);
+		per=per>100?100:per;
+		loadPer.css({height:per+'%'});
+		if(per==100) setTimeout(init_handler,200);
+		else setTimeout(load_timer,33,per);
+	}//edn func
 	
 	//----------------------------------------加载声音及处理----------------------------------------
 	var soundList={},soundMax=0,soundLoaded=0;
