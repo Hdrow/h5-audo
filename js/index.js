a79bc8f13f16e81bcf4cb0520e4bd43f1e7e28c6
cloud <cloud.chen@looper.hk>
2016-06-16 14:57:58 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 	//----------------------------------------加载页面图片----------------------------------------
 	function load_handler(){
 		var loader = new PxLoader();
-		loader.addImage('images/common/turn.gif');
+		loader.addImage('images/common/turn.png');
 		
 		//实际加载进度
 //		loader.addProgressListener(function(e) {
