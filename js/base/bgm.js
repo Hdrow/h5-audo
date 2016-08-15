647a81cfddc35a068ce0cdfc18a4dd784df90955
cloud <cloud.chen@looper.hk>
2016-08-15 14:45:46 +0800

    更新 bgm.js

--- js/base/bgm.js
+++ js/base/bgm.js
@@ -1,20 +1,28 @@
 //2016.8.15
-
 var ibgm=importBgm();
 
 function importBgm(){
 	var bgm={};
+	var defaults = {webAudio:0,src:''};
+	var opts={};
 	
 	bgm.init=function(options){
-		var defaults = {webAudio:0,src:''};
-		var opts = $.extend(defaults,options);
+		opts = $.extend(defaults,options);
 		if(opts.src!=''){
 			ibase.creatNode('a',null,'bgmBtn',null);
 			bgm.audio=iaudio.bgm({src:opts.src,onLoaded:opts.onLoad,webAudio:opts.webAudio});
 		}//edn if
 	}//edn func
 	
+	bgm.href=function(url){
+		if(url && url!=''){
+			sessionStorage.bgmPlay=ibgm.audio.bgmPlay;
+			var bgmTime=opts.webAudio?ibgm.audio.currentTime+ibgm.audio.context.currentTime-ibgm.audio.startTime:ibgm.audio.audio.currentTime;
+			sessionStorage.bgmTime=ibgm.audio.bgmPlay?bgmTime:ibgm.audio.currentTime;
+			location.href=url;
+		}//edn func
+	}//edn func
+	
 	return bgm;
 	
 }//edn func
-
