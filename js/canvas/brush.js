a4e8a24c3eb55b16955a28bd52fddd950dd01bb9
cloud <cloud.chen@looper.hk>
2017-07-10 14:29:19 +0800

    更新

--- js/canvas/brush.js
+++ js/canvas/brush.js
@@ -1,7 +1,10 @@
 function ibrush(options) {
 	var defaults = {
 		composition: 'destination-out',
-		ease: true
+		ease: true,
+		fromScale:0.5,
+		toScale:3,
+		duration:5
 	};
 	var opts = $.extend(defaults, options);
 	this._canvas = opts.canvas;
@@ -11,22 +14,21 @@ function ibrush(options) {
 	this._onUpdate = opts.onUpdate;
 	this._onComplete = opts.onComplete;
 	this._animationReq = null;
-	this._currScale = 0;
+	this._fromScale=opts.fromScale;
+	this._toScale=opts.toScale;
+	this._duration=opts.duration;
 } //edn func
 
 ibrush.prototype = {
-	on: function(coords, fromScale, toScale, duration) {
-		fromScale = fromScale != null ? fromScale : 1;
-		toScale = toScale != null ? toScale : 2.5;
-		duration = duration || 5;
+	on: function(coords) {
 		var currImageId = 0;
-		this._currScale = fromScale;
+		this._currScale = this._fromScale;
 		var _time = 0;
-		draw(this);
+		if( this._canvas && this._image && coords) draw(this);
 
 		function draw(_this) {
 			_time += 1;
-			_this._currScale = _this._ease ? applyExpoEaseOut(fromScale, toScale, _time, duration) : _this._currScale += 0.1;
+			_this._currScale = _this._ease ? applyExpoEaseOut(_this._fromScale, _this._toScale, _time, _this._duration) : _this._currScale += 0.1;
 			var _w = _this._image.width * _this._currScale;
 			var _h = _this._image.height * _this._currScale;
 			var _x = coords.x - (_w / 2);
@@ -37,7 +39,7 @@ ibrush.prototype = {
 			ctx.drawImage(_this._image, _x, _y, _w, _h);
 			ctx.restore();
 			if(_this._onUpdate) _this._onUpdate();
-			if(_this._currScale < toScale) _this._animationReq = requestAnimationFrame(function() {
+			if(_this._currScale < _this._toScale) _this._animationReq = requestAnimationFrame(function() {
 				draw(_this);
 			});
 			else {
