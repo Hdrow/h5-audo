7980e7a520558c9024d842b6be8ff94af805ae43
cloud <cloud.chen@looper.hk>
2017-11-15 12:46:00 +0800

    更新

--- js/index.landscape.js
+++ js/index.landscape.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
 	
 	//----------------------------------------页面监测代码----------------------------------------
 	function monitor_handler(){
-//		imonitor.add({obj:$('a.btnTest'),action:'touchstart',category:'',label:'测试按钮'});
+//		imonitor.add({obj:$('a.btnTest'),action:'touchstart',category:'default',label:'测试按钮'});
 	}//end func
 	
 });//end ready
